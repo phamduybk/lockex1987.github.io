ec34c6f645292c5e060a218687e73713dd787cf3
lockex1987 <lockex1987@gmail.com>
2018-04-28 20:05:22 +0700

    Add random girl image

--- js/posts.js
+++ js/posts.js
@@ -31,52 +31,53 @@ const CAT_THUMBS = {
 
 // Tất cả dữ liệu
 var allPosts = [
-    //{ lang: 'vi', cat: '', link: 'posts//', title: '', desc: '', tags: ['', ''] },
-    { lang: 'vi', cat: 'javascript', link: 'posts/javascript - get parameter/', title: 'Get Parameter', desc: 'Lấy giá trị tham số từ URL bằng JavaScript', tags: ['javascript'] },
-    { lang: 'vi', cat: 'css', link: 'posts/css - real layout examples with flexbox/', title: 'Xây dựng layout thực tế với flexbox', desc: 'Một hướng dẫn hay về xây dựng layout thực tế với flexbox trên YouTube', tags: ['css', 'flexbox', 'til'] },
-    { lang: 'vi', cat: 'css', link: 'posts/css - flexbox introduction/', title: 'Giới thiệu Flexbox', desc: 'Giới thiệu Flexbox để xây dựng bố cục trang', tags: ['css', 'flexbox', 'layout'] },
-    { lang: 'en', cat: 'web', link: 'posts/ui - slide show/', title: 'Slide show', desc: 'Slide show animation', tags: ['web'] },
-    { lang: 'en', cat: 'web', link: 'posts/ui - zoom/', title: 'Zoom', desc: 'Zoom animation', tags: ['web'] },
-    { lang: 'en', cat: 'web', link: 'posts/ui - wedding photos/', title: 'Wedding Photos', desc: 'Wedding photos of Huyên and Dương', tags: ['web'] },
-    { lang: 'en', cat: 'nodejs', link: 'https://huyen-duong.herokuapp.com/', title: 'Compay Chat', desc: 'A web-based chat application, run on NodeJS', tags: ['web', 'nodejs'], newTab: true },
-    { lang: 'en', cat: 'web', link: 'posts/project - map/', title: 'My Position', desc: 'Share your location', tags: ['web'], newTab: true },
-    { lang: 'en', cat: 'web', link: 'https://addons.mozilla.org/en-US/firefox/addon/youtube-subtitle-downloader/', title: 'Youtube Subtitle Downloader', desc: 'Firefox add-on to download subtitles from Youtube', tags: ['web'], newTab: true },
-    { lang: 'vi', cat: 'javascript', link: 'posts/lib - noti/', title: 'Noti', desc: 'Một thư viện thông báo', tags: ['web'] },
-    { lang: 'vi', cat: 'javascript', link: 'posts/lib - pagi/', title: 'Pagi', desc: 'Một thư viện phân trang', tags: ['web'] },
-    { lang: 'en', cat: 'web', link: 'posts/project - homepage/', title: 'Home page', desc: 'List of websites, motorbike alert, calendar', tags: ['web'], newTab: true },
-    { lang: 'en', cat: 'css', link: 'posts/ui - 3d css book covers/', title: '3D CSS Book Covers', desc: '3D cover', tags: ['web', 'css'] },
-    { lang: 'en', cat: 'css', link: 'posts/ui - anagram animation/', title: 'Anagram Animation', desc: 'Anagram animation of Lord Voldemort', tags: ['web', 'css'] },
-    { lang: 'en', cat: 'css', link: 'posts/ui - blend video background/', title: 'Blend Video Background', desc: 'Blend video background', tags: ['web'] },
-    { lang: 'en', cat: 'web', link: 'posts/project - comic reader/', title: 'Comic Reader', desc: 'Comic reader', tags: ['web'] },
-    { lang: 'en', cat: 'web', link: 'posts/ui - gallery/', title: 'Gallery', desc: 'A simple gallery', tags: ['web'] },
-    { lang: 'en', cat: 'web', link: 'posts/project - highlight syntax/', title: 'Highlight Syntax', desc: 'Highlight syntax of source code', tags: ['web'] },
-    { lang: 'en', cat: 'web', link: 'posts/project - markdown preview/', title: 'Markdown preview', desc: 'Markdown preview', tags: ['web'] },
-    { lang: 'en', cat: 'javascript', link: 'posts/javascript - password generator/', title: 'Password Generator', desc: 'Password generator', tags: ['javascript'] },
-    { lang: 'en', cat: 'web', link: 'posts/project - puzzle/', title: 'Puzzle', desc: 'A puzzle game', tags: ['web'] },
-    { lang: 'en', cat: 'web', link: 'posts/project - short/', title: 'Short funny stories', desc: 'Short stories', tags: ['web'] },
-    { lang: 'en', cat: 'git', link: 'posts/git - save git password/', title: 'Save git password', desc: 'Save git password so you don\'t have to keep typing it when you pull/push something', tags: ['git'] },
-    { lang: 'vi', cat: 'other', link: 'posts/buy pc/', title: 'Buy a new PC', desc: 'Mua một máy tính mới', tags: ['other'] },
-    { lang: 'en', cat: 'linux', link: 'posts/linux - install oracle jdk/', title: 'Install JDK', desc: 'Replace OpenJDK with OracleJDK on Linux', tags: ['linux', 'java'] },
-    { lang: 'vi', cat: 'web', link: 'posts/web - social share/', title: 'Social share', desc: 'Thêm link chia sẻ qua Facebook, Google+, Twitter, LinkedIn, Pinterest, Email', tags: ['web'] },
-    { lang: 'en', cat: 'javascript', link: 'posts/web - string format function/', title: 'String format function', desc: 'An utility function', tags: ['javascript'] },
-    { lang: 'en', cat: 'javascript', link: 'posts/web - add css dynamically/', title: 'Add CSS dynamically', desc: 'Add CSS dynamically by JavaScript', tags: ['javascript', 'css'] },
-    { lang: 'en', cat: 'web', link: 'posts/web - mathml/', title: 'MathML', desc: 'Display math equations on web', tags: ['web'] },
-    { lang: 'en', cat: 'css', link: 'posts/bootstrap - media/', title: 'Bootstrap Media', desc: 'Bootstrap Media tutorial', tags: ['css', 'bootstrap'] },
-    { lang: 'vi', cat: 'css', link: 'posts/bootstrap - form/', title: 'Bootstrap Form', desc: 'Hướng dẫn thiết kế form với Bootstrap', tags: ['css', 'bootstrap'] },
-    { lang: 'en', cat: 'web', link: 'posts/web - popular cdn/', title: 'Popular CDN', desc: 'Some popular CDN links', tags: ['web'] },
-    { lang: 'vi', cat: 'javascript', link: 'posts/web - jquery-comments/', title: 'jquery-comments', desc: 'Một thư viện về comment rất hay', tags: ['web'] },
-    { lang: 'vi', cat: 'java', link: 'posts/tomcat - serving static content from outside tomcat/', title: 'Serving static content from outside Tomcat', desc: 'Cung cấp nội dung static từ bên ngoài webapps sử dụng Tomcat', tags: ['java'] },
-    { lang: 'vi', cat: 'java', link: 'posts/java - fix smtps error when sending mail/', title: 'Fix smtps error when sending mail', desc: 'Sửa lỗi gửi mail khi kết nối đến server HTTPS', tags: ['java'] },
-    { lang: 'vi', cat: 'eclipse', link: 'posts/eclipse - disable join comment lines/', title: 'Disable join comment lines in Eclipse', desc: 'Cấu hình Eclipse không nối các dòng comment khi format code', tags: ['eclipse'] },
-    { lang: 'vi', cat: 'eclipse', link: 'posts/eclipse - shortcuts/', title: 'Eclipse Shortcut', desc: 'Những phím tắt của Eclipse', tags: ['eclipse'] },
-    { lang: 'en', cat: 'maven', link: 'posts/maven - latest maven repositories/', title: 'Latest Maven repositories', desc: 'I will use these latest libraries in all my Java projects', tags: ['maven'] },
-    { lang: 'en', cat: 'less', link: 'posts/web - less/', title: 'LESS', desc: 'A CSS Pre-processor', tags: ['less'] },
-    { lang: 'en', cat: 'javascript', link: 'posts/web - get computed css/', title: 'Get Computed CSS', desc: 'Get computed CSS', tags: ['javascript'] },
-    { lang: 'en', cat: 'web', link: 'posts/web - highlightjs/', title: 'highlight.js', desc: 'Highlight source code syntax', tags: ['web'] },
-    { lang: 'en', cat: 'other', link: 'posts/knowledge - bake a potato in the microwave/', title: 'How to Bake a Potato in the Microwave', desc: 'Bake a potato in the microwave', tags: ['other'] },
-    { lang: 'en', cat: 'other', link: 'posts/web - personal website/', title: 'Personal Website', desc: 'Create your own website', tags: ['other'] },
-    { lang: 'vi', cat: 'web', link: 'posts/web - simple template/', title: 'Simple Template', desc: 'A template engine without server script (AJAX only)', tags: ['web'] },
-    { lang: 'vi', cat: 'linux', link: 'posts/linux - resize, convert and modify images/', title: 'Resize, Convert and Modify Images', desc: 'Manipulating images', tags: ['linux'] }
+    //{ lang: 'vi', cat: '', link: 'posts//', title: '', tags: ['', ''] },
+    { lang: 'vi', cat: 'javascript', link: 'posts/ui - random girl image/', title: 'Hiển thị ngẫu nhiên ảnh girl xinh', tags: ['javascript', 'css'] },
+    { lang: 'vi', cat: 'javascript', link: 'posts/javascript - get parameter/', title: 'Lấy giá trị tham số từ URL bằng JavaScript', tags: ['javascript'] },
+    { lang: 'vi', cat: 'css', link: 'posts/css - real layout examples with flexbox/', title: 'Xây dựng layout thực tế với flexbox', tags: ['css', 'flexbox', 'til'] },
+    { lang: 'vi', cat: 'css', link: 'posts/css - flexbox introduction/', title: 'Giới thiệu Flexbox', tags: ['css', 'flexbox', 'layout'] },
+    { lang: 'en', cat: 'web', link: 'posts/ui - slide show/', title: 'Slide show animation', tags: ['web'] },
+    { lang: 'en', cat: 'web', link: 'posts/ui - zoom/', title: 'Zoom animation', tags: ['web'] },
+    { lang: 'en', cat: 'web', link: 'posts/ui - wedding photos/', title: 'Wedding photos of Huyên and Dương', tags: ['web'] },
+    { lang: 'en', cat: 'nodejs', link: 'https://huyen-duong.herokuapp.com/', title: 'A web-based chat application run with NodeJS', tags: ['nodejs', 'chat'], newTab: true },
+    { lang: 'en', cat: 'web', link: 'posts/project - map/', title: 'Share your location with Google Map', tags: ['map'], newTab: true },
+    { lang: 'en', cat: 'web', link: 'https://addons.mozilla.org/en-US/firefox/addon/youtube-subtitle-downloader/', title: 'Firefox add-on to download subtitles from Youtube', tags: ['web'], newTab: true },
+    { lang: 'vi', cat: 'javascript', link: 'posts/lib - noti/', title: 'Noti - Một thư viện thông báo', tags: ['web'] },
+    { lang: 'vi', cat: 'javascript', link: 'posts/lib - pagi/', title: 'Pagi - Một thư viện phân trang', tags: ['web'] },
+    { lang: 'en', cat: 'web', link: 'posts/project - homepage/', title: 'Customized home page (websites, calendar,..)', tags: ['web'], newTab: true },
+    { lang: 'en', cat: 'css', link: 'posts/ui - 3d css book covers/', title: '3D CSS book covers', tags: ['web', 'css', '3d'] },
+    { lang: 'en', cat: 'css', link: 'posts/ui - anagram animation/', title: 'Anagram animation', tags: ['web', 'css'] },
+    { lang: 'en', cat: 'css', link: 'posts/ui - blend video background/', title: 'Blend video background', tags: ['web'] },
+    { lang: 'en', cat: 'web', link: 'posts/project - comic reader/', title: 'My comic reader', tags: ['web'] },
+    { lang: 'en', cat: 'web', link: 'posts/ui - gallery/', title: 'A simple gallery', tags: ['web'] },
+    { lang: 'en', cat: 'web', link: 'posts/project - highlight syntax/', title: 'Highlight syntax of source code', tags: ['web'] },
+    { lang: 'en', cat: 'web', link: 'posts/project - markdown preview/', title: 'Markdown preview', tags: ['web'] },
+    { lang: 'en', cat: 'javascript', link: 'posts/javascript - password generator/', title: 'Password Generator', tags: ['javascript'] },
+    { lang: 'en', cat: 'web', link: 'posts/project - puzzle/', title: 'A puzzle game', tags: ['web'] },
+    { lang: 'en', cat: 'web', link: 'posts/project - short/', title: 'Short funny stories', tags: ['web'] },
+    { lang: 'en', cat: 'git', link: 'posts/git - save git password/', title: 'Save git password', tags: ['git'] },
+    { lang: 'vi', cat: 'other', link: 'posts/buy pc/', title: 'Mua một máy tính mới', tags: ['other'] },
+    { lang: 'en', cat: 'linux', link: 'posts/linux - install oracle jdk/', title: 'Replace OpenJDK with OracleJDK on Linux', tags: ['linux', 'java'] },
+    { lang: 'vi', cat: 'web', link: 'posts/web - social share/', title: 'Thêm link chia sẻ qua mạng xã hội, email', tags: ['web'] },
+    { lang: 'en', cat: 'javascript', link: 'posts/web - string format function/', title: 'String format - utility function', tags: ['javascript'] },
+    { lang: 'en', cat: 'javascript', link: 'posts/web - add css dynamically/', title: 'Add CSS dynamically by JavaScript', tags: ['javascript', 'css'] },
+    { lang: 'en', cat: 'web', link: 'posts/web - mathml/', title: 'Display math equations on web with MathML', tags: ['web'] },
+    { lang: 'en', cat: 'css', link: 'posts/bootstrap - media/', title: 'Bootstrap Media tutorial', tags: ['css', 'bootstrap'] },
+    { lang: 'vi', cat: 'css', link: 'posts/bootstrap - form/', title: 'Hướng dẫn thiết kế form với Bootstrap', tags: ['css', 'bootstrap'] },
+    { lang: 'en', cat: 'web', link: 'posts/web - popular cdn/', title: 'Some popular CDN links', tags: ['web'] },
+    { lang: 'vi', cat: 'javascript', link: 'posts/web - jquery-comments/', title: 'jquery-comments - một thư viện về comment rất hay', tags: ['web'] },
+    { lang: 'vi', cat: 'java', link: 'posts/tomcat - serving static content from outside tomcat/', title: 'Cung cấp nội dung static từ bên ngoài webapps sử dụng Tomcat', tags: ['java'] },
+    { lang: 'vi', cat: 'java', link: 'posts/java - fix smtps error when sending mail/', title: 'Sửa lỗi gửi mail khi kết nối đến server HTTPS', tags: ['java'] },
+    { lang: 'vi', cat: 'eclipse', link: 'posts/eclipse - disable join comment lines/', title: 'Cấu hình Eclipse không nối các dòng comment khi format code', tags: ['eclipse'] },
+    { lang: 'vi', cat: 'eclipse', link: 'posts/eclipse - shortcuts/', title: 'Những phím tắt của Eclipse', tags: ['eclipse'] },
+    { lang: 'en', cat: 'maven', link: 'posts/maven - latest maven repositories/', title: 'Latest Maven repositories', tags: ['maven'] },
+    { lang: 'en', cat: 'less', link: 'posts/web - less/', title: 'LESS - a CSS pre-processor', tags: ['less'] },
+    { lang: 'en', cat: 'javascript', link: 'posts/web - get computed css/', title: 'Get Computed CSS', tags: ['javascript'] },
+    { lang: 'en', cat: 'web', link: 'posts/web - highlightjs/', title: 'Highlight source code syntax with highlight.js', tags: ['web'] },
+    { lang: 'en', cat: 'other', link: 'posts/knowledge - bake a potato in the microwave/', title: 'How to Bake a Potato in the Microwave', tags: ['other'] },
+    { lang: 'en', cat: 'other', link: 'posts/web - personal website/', title: 'Create your own personal website', tags: ['other'] },
+    { lang: 'vi', cat: 'web', link: 'posts/web - simple template/', title: 'A template engine without server script (AJAX only)', tags: ['web'] },
+    { lang: 'vi', cat: 'linux', link: 'posts/linux - resize, convert and modify images/', title: 'Resize, Convert and Modify Images', tags: ['linux'] }
 ];
 
 // Cập nhật lại ảnh cho tất cả bài viết
@@ -121,6 +122,7 @@ var pag = new Pagi({
     showNumbers: false,
     showTotalNumber: false,
     showNoRecordText: true,
+    noRecordText: 'No post found',
     previousText: '← NEWER',
     nextText: 'OLDER →',
     callbackFunc: gotoPage,
@@ -131,7 +133,7 @@ var app = new Vue({
     el: '#app',
     data: {
         posts: [],
-        pageTitle: 'ls /posts'
+        pageTitle: 'List of posts'
     }
 });
 
@@ -141,7 +143,7 @@ if (textQuery) {
     textQuery = textQuery.toLowerCase();
     filterPosts = [];
     allPosts.forEach(p => {
-        if (p.title.toLowerCase().includes(textQuery) || p.desc.toLowerCase().includes(textQuery) || p.link.toLowerCase().includes(textQuery)) {
+        if (p.title.toLowerCase().includes(textQuery) || p.link.toLowerCase().includes(textQuery)) {
             filterPosts.push(p);
         }
     });
@@ -159,6 +161,26 @@ if (textQuery) {
     filterPosts = allPosts;
 }
 
-// Vào trang sẽ chuyển đến thứ nhất
-gotoPage(1);
+window.addEventListener("DOMContentLoaded", function() {
+    // Vào trang sẽ chuyển đến thứ nhất
+    gotoPage(1);
+});
 
+/**
+ * Lấy kích thước trình duyệt có phải
+ * @returns trả về một số, là chiều rộng của trình duyệt ở đơn vị pixel
+ */
+function getBrowserWidth() {
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    //var width = $(window).width();
+    return width;
+}
+
+window.addEventListener("load", function() {
+    // Vì chỗ load ảnh này chậm nên trình duyệt sẽ hiển thị loading lâu
+    // Thêm vào sự kiện "load" để chờ khi trình duyệt không hiển thị loading nữa
+    // Ngoài ra chỉ khi kích thước trình duyệt lớn thì mới hiển thị
+    if (getBrowserWidth() >= 768) {
+        document.querySelector(".sidebar img").src = "https://thecatapi.com/api/images/get?size=small";
+    }
+});
