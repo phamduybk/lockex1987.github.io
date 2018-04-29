4fdf18db272eada04916a5857db63eb128fad28f
lockex1987 <lockex1987@gmail.com>
2018-04-29 14:17:40 +0700

    Add Owl Carousel

--- posts/project - comic reader/js/comic.js
+++ posts/project - comic reader/js/comic.js
@@ -22,6 +22,22 @@ var images;
 // Danh sách các chapter
 var chapterList;
 
+// Vùng hiển thị danh sách
+var comicInfo = $("#comicInfo");
+
+// Vùng xem
+var chapterViewer = $("#chapterViewer");
+
+// Index của chapter hiện tại
+var curIdx = -1;
+
+// Index của chương đang đọc, lưu ở localStorage
+var savedIdx;
+
+//addSwipeEvent();
+//$("#viewer").owlCarousel();
+var owl = $(".owl-carousel").owlCarousel({ items: 1, autoHeight:true });
+
 // Lấy danh sách chapter và bind ra để người dùng chọn
 $.getJSON("data/chapters.json", function(data) {
 	chapterList = data;
@@ -34,24 +50,69 @@ $.getJSON("data/chapters.json", function(data) {
 	}
 	$("#list").html(ul);
 	currentChapter.html(select);
+
+	savedIdx = localStorage.savedIdx;
+	
+	bindSavedInfo();
+
+	// Bind event
+
 });
 
-// Vùng hiển thị danh sách
-var comicInfo = $("#comicInfo");
+function gotoCurrentSavedChapter() {
+	viewChapter(savedIdx);
+}
 
-// Vùng xem
-var chapterViewer = $("#chapterViewer");
+function gotoNextSavedChapter() {
+	viewChapter(savedIdx + 1);
+}
 
-// Index của chapter hiện tại
-var curIdx = -1;
+function gotoPreviousSavedChapter() {
+	viewChapter(savedIdx - 1);
+}
+
+function bindSavedInfo() {
+	console.log("savedIdx:", savedIdx);
+	if (savedIdx != undefined && savedIdx != null) {
+		$("#savedInfo").show();
+
+		// Trong trường hợp lưu ở localStorage thì là kiểu String
+		// Cần chuyển về kiểu số để tính toán cho đúng
+		savedIdx = parseInt(savedIdx);
+
+		$("#savedInfo p:nth-child(1) a").show().text(chapterList[savedIdx].title);
+		if (savedIdx == chapterList.length - 1) {
+			$("#savedInfo p:nth-child(2)").hide();
+		} else {
+			$("#savedInfo p:nth-child(2) a").show().text(chapterList[savedIdx + 1].title);
+		}
+		if (savedIdx == 0) {
+			$("#savedInfo p:nth-child(3)").hide();
+		} else {
+			$("#savedInfo p:nth-child(3) a").show().text(chapterList[savedIdx - 1].title);
+		}
+	} else {
+		$("#savedInfo").hide();
+	}
+}
+
+
+
 
 /**
  * Hiển thị chapter nào đó
  * @param idx Chỉ số index của chapter
  */
 function viewChapter(idx) {
+	// Ẩn hiện
 	comicInfo.hide();
 	chapterViewer.show();
+
+	// Lưu lịch sử
+	savedIdx = idx;
+	localStorage.savedIdx = savedIdx;
+	console.log(savedIdx);
+	bindSavedInfo();
 
 	// Nếu là chuyển sang chương khác thì mới load lại
 	if (curIdx != idx) {
@@ -77,6 +138,14 @@ function getImages() {
  * @param data Danh sách ảnh
  */
 function initNewChapter(data) {
+	if (images) {
+		//viewer.empty();
+		for (var i = 0; i < images.length; i++) {
+			owl.trigger('remove.owl.carousel', i).trigger('refresh.owl.carousel');
+		}
+	}
+	
+
 	links = data;
 	current = 0;
 	images = [];
@@ -87,10 +156,6 @@ function initNewChapter(data) {
 	readProgress.max = links.length;
 	readProgress.value = 1;
 
-	// 
-	viewer.empty();
-	//index = 0;
-
 	loadImage(0, curIdx);
 }
 
@@ -100,29 +165,31 @@ function loadImage(index, chap) {
 			var img = new Image();
 
 			// Sau khi load xong thì load ảnh tiếp theo
+			var nextIndex = index + 1;
 			img.onload = function() {
-				index++;
-				loadProgress.value = index;
-				loadImage(index, chap);
+				loadProgress.value = nextIndex;
+				loadImage(nextIndex, chap);
 			};
 			img.src = links[index];
 
 			// Ẩn các ảnh tiếp theo, chỉ hiện 1 ảnh
 			if (index > 0) {
-				img.style.display = "none";
+				//img.style.display = "none";
 			}
 
 			// Thêm vào danh sách ảnh
 			images.push(img);
 
 			// Thêm vào vùng hiển thị
-			viewer.append(img);
+			//viewer.append($("<div></div>").append(img));
+
+			//console.log(img.src);
+			owl.trigger('add.owl.carousel', img).trigger('refresh.owl.carousel');
 		}
 	} else {
 		// Đã load xong
 		loadProgress.style.display = "none";
-
-		addSwipeEvent();
+		owl.trigger('refresh.owl.carousel');
 	}
 }
 
@@ -165,8 +232,7 @@ function previousChapter() {
 	if (curIdx == 0) {
 		noti.info("This is the first chapter");
 	} else {
-		curIdx--;
-		getImages();
+		gotoChapter(curIdx - 1);
 	}
 }
 
@@ -177,8 +243,7 @@ function nextChapter() {
 	if (curIdx == chapterList.length - 1) {
 		noti.info("This is the last chapter");
 	} else {
-		curIdx++;
-		getImages();
+		gotoChapter(curIdx + 1);
 	}
 }
 
@@ -186,7 +251,15 @@ function nextChapter() {
  * Chuyển đến một trang nào đó
  */
 function changeChapter() {
-	curIdx = currentChapter.val();
+	gotoChapter(currentChapter.val());
+}
+
+/**
+ * Chuyển đến một chapter nào đó.
+ * @param idx Chỉ số
+ */
+function gotoChapter(idx) {
+	curIdx = idx;
 	getImages();
 }
 
