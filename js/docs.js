5f16a6c597cf1bd6c4457b70026bf67f6275a781
lockex1987 <lockex1987@gmail.com>
2018-06-05 20:06:24 +0700

    Remove stories and privates from menu

--- js/docs.js
+++ js/docs.js
@@ -1,69 +1,25 @@
 /**
- * Tạo một request AJAX để thêm các thành phần của trang (header, footer)
- * @param divId ID vùng DIV
- * @param url URL của phần template
+ * Thêm vùng header chung.
  */
-function includeHtml(divId, url) {
-	var req = new XMLHttpRequest();
-	req.onreadystatechange = function() {
-		if (req.readyState == 4 && req.status == 200) {
-			document.getElementById(divId).innerHTML = req.responseText;
-		}
-	};
-	req.open("GET", url);
-	req.send();
-}
-
-/**
- * Kiểm tra thêm vùng DIV header và footer
- * @param divId ID vùng DIV
- * @param targetUrl URL của template
- * @param tag Tên thẻ
- * @param isFirst Thêm vào đầu hay cuối trang (header thêm ở đầu, footer thêm ở cuối)
- */
-function checkTemplate(divId, targetUrl, tag, isFirst) {
-	var node = document.createElement(tag);
-	node.id = divId;
-	var body = document.querySelector("body");
-	if (isFirst) {
-		//body.prepend(node);
-		body.insertBefore(node, body.firstChild);
-	} else {
-		//body.append(node);
-		body.appendChild(node);
-	}
-	
-	//includeHtml(divId, targetUrl);
-	if (divId == "header") {
-		node.innerHTML = `
-			        <div class="mw">
-								<div id="logo">
-									<a title="cd ~" href="/"><span>LOCKE</span><span style="">x1987</span></a>
-								</div>
-								<div id="menu">
-									<ul>
-										<li><a href="/posts.html">Posts</a></li>
-									</ul>
-
-									<ul class="private">
-										<li><a href="/privates.html">Privates</a></li>
-									</ul>
-									<ul class="private">
-										<li><a href="/stories.html">Stories</a></li>
-									</ul>
-									<ul>
-										<li><a href="/cv.html">About</a></li>
-									</ul>
-								</div>
-							</div>`;
-	} else {
-		node.innerHTML = `&copy; Since 2017`;
-	}
-	
-	// Ẩn các link riêng tư nếu là server public
-	if (divId == "header") {
-		hidePrivates();
-	}
+function addHeader() {
+	var node = document.createElement("header");
+	node.id = "header";
+	node.innerHTML = `
+		        <div class="mw">
+							<div id="logo">
+								<a title="cd ~" href="/"><span>LOCKE</span><span style="">x1987</span></a>
+							</div>
+							<div id="menu">
+								<ul>
+									<li><a href="/posts.html">Posts</a></li>
+								</ul>
+								<ul>
+									<li><a href="/cv.html">About</a></li>
+								</ul>
+							</div>
+						</div>`;
+	document.body.insertBefore(node, document.body.firstChild);
+    console.log("Add header");
 }
 
 /**
@@ -86,19 +42,6 @@ function isLocalFile() {
 	return window.location.protocol == "file:";
 }
 
-/**
- * Ẩn các link riêng tư nếu là server public.
- */
-function hidePrivates() {
-	// Neu la local thi hien thi private, con neu khong thi an di
-	if (!isLocalHost()) {
-		var privates = document.querySelectorAll(".private");
-		for (var i = 0; i < privates.length; i++) {
-			privates[i].style.display = "none";
-		}
-	}
-}
-
 function loadCss(e, t, n) {
 	var i = window.document.createElement("link");
 	var o = t || window.document.getElementsByTagName("script")[0];
@@ -113,8 +56,6 @@ function loadCss(e, t, n) {
 function addFacebookComment() {
 	// Bỏ qua trang chủ, trang danh sách, chạy local
 	if (window.location.pathname != "/"
-			&& window.location.pathname != "/projects/home/"
-			&& window.location.pathname != "/docs/home/"
 			&& window.location.hostname != "localhost"
 			&& !isLocalFile()) {
 				
@@ -150,13 +91,8 @@ function addFacebookComment() {
 // Không sử dụng "load" vì như vậy phải chờ sau khi load xong hoàn toàn trang (cả CSS, ảnh, frame)
 // Sử dụng DOMContentLoaded
 window.addEventListener("DOMContentLoaded", function() {
-	// Add more sections: header and footer
-	//var header = document.querySelector("#header");
-	//header == null && 
 	if (!isLocalFile() && !isHomePage()) {
-		// Relative path to the JS file?
-		checkTemplate("header", "../../layout/header.html", "header", true);
-		//checkTemplate("footer", "../../layout/footer.html", "footer", false);
+		addHeader();
 	}
 	
 	//addFacebookComment();
