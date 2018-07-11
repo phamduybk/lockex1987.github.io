100d71ee2d80feb1933eb6e5c6928687b12c5349
lockex1987 <lockex1987@gmail.com>
2018-07-11 19:42:07 +0700

    Add more Python

---
+++ posts/python - cgi/cgi.py
@@ -0,0 +1,9 @@
+print "Content-type:text/html\r\n\r\n"
+print '<html>'
+print '<head>'
+print '<title>Lap trinh CGI trong Python</title>'
+print '</head>'
+print '<body>'
+print '<h2>Day la chuong trinh CGI dau tien trong Python</h2>'
+print '</body>'
+print '</html>'
