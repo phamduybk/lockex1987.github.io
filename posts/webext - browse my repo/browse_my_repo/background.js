181607f11455d2872d466c07add64be213f1e6fa
lockex1987 <lockex1987@gmail.com>
2018-09-25 19:49:26 +0700

    Add browse my repo

---
+++ posts/webext - browse my repo/browse_my_repo/background.js
@@ -0,0 +1,10 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	var url = tab.url;
+	var username = 'lockex1987';
+	var oldPath = `https://github.com/${username}/${username}.github.io/`;
+	var newPath = `https://${username}.github.io/`;
+	var redirectUrl = url.replace(oldPath, newPath)
+            .replace('/tree/master/', '/');
+            .replace('/blob/master/', '/');
+	chrome.tabs.update(tab.id, { url: redirectUrl });
+});
