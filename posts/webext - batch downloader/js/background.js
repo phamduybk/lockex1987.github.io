d4d993bc59275f9c185ec84ab8e0ddb60aeb54b7
lockex1987 <lockex1987@gmail.com>
2018-12-29 11:01:39 +0700

    Update style and batch downloader

--- posts/webext - batch downloader/js/background.js
+++ posts/webext - batch downloader/js/background.js
@@ -68,3 +68,18 @@ chrome.downloads.onChanged.addListener(function(downloadDelta) {
 	}
 	sendStats();
 });
+
+function openMainPage() {
+	var optionsUrl = chrome.extension.getURL('popup.html');
+	chrome.tabs.query({ url: optionsUrl }, function(tabs) {
+		if (tabs.length) {
+		chrome.tabs.update(tabs[0].id, { active: true });
+		} else {
+		chrome.tabs.create({url: optionsUrl});
+		}
+	});
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	openMainPage();
+});
