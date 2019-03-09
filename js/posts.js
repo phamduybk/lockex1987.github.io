2517ab3afda4fee732ec79388022e418f59391bb
lockex1987 <lockex1987@gmail.com>
2019-03-09 10:23:19 +0700

    Remove chart on posts page

--- js/posts.js
+++ js/posts.js
@@ -320,7 +320,7 @@ function buildCategories() {
 window.addEventListener("DOMContentLoaded", function() {
     updateThumbnailImage();
     filterAndUpdatePageTitle();
-    buildChart();
+    //buildChart();
     buildCategories();
 });
 
