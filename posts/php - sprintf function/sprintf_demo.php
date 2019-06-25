e026956dd2e2bb6da972d28914e08e7a4250b32b
Nguyễn Văn Huyên <lockex1987@gmail.com>
2019-06-25 13:42:58 +0700

    Update sprintf_demo.php

--- posts/php - sprintf function/sprintf_demo.php
+++ posts/php - sprintf function/sprintf_demo.php
@@ -7,3 +7,6 @@ $interval = 'day';
 $url_template = 'http://10.30.154.10:8500/voice/api/log/v1/rest/overview?start_at=%s&end_at=%s&interval=%s';
 $url = sprintf($url_template, $start_at, $end_at, $interval);
 echo $url;
+
+sprintf('%08d', 1234567);
+str_pad($value, 8, '0', STR_PAD_LEFT);
